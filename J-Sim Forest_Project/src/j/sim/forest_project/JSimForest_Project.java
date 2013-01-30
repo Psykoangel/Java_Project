086078769f77d6f4ca3e215f6a3663d5fbf9e96a
Psyko <Psyko@Samael>
2013-01-30 22:37:20 +0100

    Initialize of the first step of the project.

---
+++ J-Sim Forest_Project/src/j/sim/forest_project/JSimForest_Project.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j.sim.forest_project;
+
+/**
+ *
+ * @author Psyko
+ */
+public class JSimForest_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
