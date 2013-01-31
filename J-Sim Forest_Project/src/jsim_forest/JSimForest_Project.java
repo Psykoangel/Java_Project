6ebcd8eb35e7c53711d74d6b400b1eed953fd705
Psyko <Psyko@Samael>
2013-01-31 09:30:04 +0100

    Root Code cleaning

--- J-Sim Forest_Project/src/jsim_forest/JSimForest_Project.java
+++ J-Sim Forest_Project/src/jsim_forest/JSimForest_Project.java
@@ -1,19 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package jsim_forest;
 
-/**
- *
- * @author Psyko
- */
 public class JSimForest_Project {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
