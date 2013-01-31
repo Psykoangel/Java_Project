b5fb6f14e4803b40ee0eaa98a5349c8dc75e6e72
Psyko <Psyko@Samael>
2013-01-31 10:10:28 +0100

    Code testing 01

--- J-Sim Forest_Project/src/jsim_forest/JSimForest_Project.java
+++ J-Sim Forest_Project/src/jsim_forest/JSimForest_Project.java
@@ -6,5 +6,7 @@ public class JSimForest_Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("=== J-sim Forest ===");
+        System.out.println("Text test : this exists only to test the git commit function !");
     }
 }
