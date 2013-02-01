b463a204789a3caba9f14df67c12f13cfa6ab4dd
MicroSky <alexandre.gass@viacesi.fr>
2013-02-01 17:39:38 +0100

    Debut interface

--- J-Sim Forest_Project/src/gil/GILObject.java
+++ J-Sim Forest_Project/src/gil/GILObject.java
@@ -1,15 +1,33 @@
 
 package gil;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GILObject extends JFrame {
+    private JPanel pan;
+    private JPanel pan1;
     
     public GILObject() {
-        this.setTitle("Main");
-        this.setSize(800,600);
+        this.setTitle("J-Sim");
+        this.setSize(500,300);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.pan = new JPanel(new BorderLayout());
+        this.pan.setBackground(Color.RED);
+        
+        this.pan1 = new JPanel();
+        this.pan1.setBackground(Color.BLUE);
+/*        pan.add(pan1, BorderLayout.EAST);*/
+        
+      /*  this.getContentPane().add(pan);
+        this.pan.add(new Button("OK"));
+        this.getContentPane().add(pan1);*/
+        
         this.setVisible(true);
     }
 }
