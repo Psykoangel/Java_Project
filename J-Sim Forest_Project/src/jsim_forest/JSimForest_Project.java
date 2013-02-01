f3222877146a470095112c0f968d15671eb0da49
Psyko <Psyko@Samael>
2013-02-01 10:58:32 +0100

    Test Loop for Psyko's Branch

--- J-Sim Forest_Project/src/jsim_forest/JSimForest_Project.java
+++ J-Sim Forest_Project/src/jsim_forest/JSimForest_Project.java
@@ -9,5 +9,9 @@ public class JSimForest_Project {
         System.out.println("=== J-sim Forest ===");
         System.out.println("Text test : this exists only to test the git commit function !");
         System.out.println("Test branch master1");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Ce truc me fait vraiment chier !!!");
+        }
     }
 }
