bef2cb9320ee52434f51262a5a8ea7b1c6cac58c
Psyko <Psyko@Samael>
2013-02-04 09:20:31 +0100

    BOLObject VecinityCount

--- J-Sim Forest_Project/src/bol/BOLObject.java
+++ J-Sim Forest_Project/src/bol/BOLObject.java
@@ -1,6 +1,8 @@
 
 package bol;
 
+import java.util.*;
+
 public class BOLObject {
     
     private Tableau tab;
@@ -19,17 +21,106 @@ public class BOLObject {
         return tab;
     }
     
-    public void CheckTab(Tableau t){
-        for (int width = 0; width < t.getY(); width++) {
-            for (int length = 0; length < t.getX(); length++) {
-                Case c = t.getTab()[length][width];
-                // TODO complete the fonction checkTab.
-                //Foreach cells look at the up, right, down, left and diagonals to list the various types of cells.
+    public void CheckTab(){
+        Case cRight = null;
+        Case cRightDown = null;
+        Case cDown = null;
+        Case cDownLeft = null;
+        Case cLeft = null;
+        Case cLeftUp = null;
+        Case cUp = null;
+        Case cRightUp = null;
+        List<Case> VecinityList = new ArrayList<>();
+        
+        for (int width = 0; width < tab.getY() -1; width++) {
+            for (int length = 0; length < tab.getX() -1; length++) {
+                Case c = tab.getTab()[length][width];
+                
+                if (length < tab.getX() -1) {
+                    cRight = tab.getTab()[length + 1][width];
+                    VecinityList.add(cRight);
+                }
+                if (length < tab.getX() -1 && width < tab.getY() -1) {
+                    cRightDown = tab.getTab()[length + 1][width + 1];
+                    VecinityList.add(cRightDown);
+                }
+                if (width < tab.getY() -1) {
+                    cDown = tab.getTab()[length][width + 1];
+                    VecinityList.add(cDown);
+                }
+                if (length > 0 && width < tab.getY() -1) {
+                    cDownLeft = tab.getTab()[length - 1][width + 1];
+                    VecinityList.add(cDownLeft);
+                }
+                if (length > 0) {
+                    cLeft = tab.getTab()[length - 1][width];
+                    VecinityList.add(cLeft);
+                }
+                if (length > 0 && width > 0) {
+                    cLeftUp = tab.getTab()[length - 1][width - 1];
+                    VecinityList.add(cLeftUp);
+                }
+                if (width > 0) {
+                    cUp = tab.getTab()[length][width - 1];
+                    VecinityList.add(cUp);
+                }
+                if (length < tab.getX() -1 && width > 0) {
+                    cRightUp = tab.getTab()[length + 1][width - 1];
+                    VecinityList.add(cRightUp);
+                }
+                
             }
         }
     }
     
-    private void VecinityCellsCheck(){//   Fait la vérification du voisinage d'une cellule
+    private HashMap VecinityCountCheck(ArrayList l){
+        HashMap countList = new HashMap();
+        countList.put(Etat.vide, 0);
+        countList.put(Etat.jeunePousse, 0);
+        countList.put(Etat.arbuste, 0);
+        countList.put(Etat.arbre, 0);
         
+        for (Iterator it = l.iterator(); it.hasNext();) {
+            Case c = (Case) it.next();
+            switch(c.getEtat()){
+                case vide:
+                    if (countList.containsKey(Etat.vide)) {
+                        int temp = Integer.valueOf(countList.get(Etat.vide).toString());
+                        temp++;
+                        countList.put(Etat.vide, temp);
+                    } else {
+                        countList.put(Etat.vide, 1);
+                    }
+                break;
+                case jeunePousse:
+                    if (countList.containsKey(Etat.jeunePousse)) {
+                        int temp = Integer.valueOf(countList.get(Etat.jeunePousse).toString());
+                        temp++;
+                        countList.put(Etat.jeunePousse, temp);
+                    } else {
+                        countList.put(Etat.jeunePousse, 1);
+                    }
+                break;
+                case arbuste:
+                    if (countList.containsKey(Etat.arbuste)) {
+                        int temp = Integer.valueOf(countList.get(Etat.arbuste).toString());
+                        temp++;
+                        countList.put(Etat.arbuste, temp);
+                    } else {
+                        countList.put(Etat.arbuste, 1);
+                    }
+                break;
+                case arbre:
+                    if (countList.containsKey(Etat.arbre)) {
+                        int temp = Integer.valueOf(countList.get(Etat.arbre).toString());
+                        temp++;
+                        countList.put(Etat.arbre, temp);
+                    } else {
+                        countList.put(Etat.arbre, 1);
+                    }
+                break;
+            }
+        }
+        return countList;
     }
 }
---
+++ J-Sim Forest_Project/src/bol/Neighborhood.java
@@ -0,0 +1,13 @@
+
+package bol;
+
+public enum Neighborhood {
+    Moore(0),
+    VonNeumann(1);
+    
+    private int valeur;
+    
+    private Neighborhood(int id){
+        this.valeur = id;
+    }
+}
