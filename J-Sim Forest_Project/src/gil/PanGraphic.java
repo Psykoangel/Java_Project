75b908f1c9576857807c8c2b7113a527a731f76e
MicroSky <alexandre.gass@viacesi.fr>
2013-02-05 09:48:23 +0100

    Interface -> grid finised

--- J-Sim Forest_Project/src/gil/PanGraphic.java
+++ J-Sim Forest_Project/src/gil/PanGraphic.java
@@ -1,36 +1,35 @@
 package gil;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class PanGraphic extends JPanel{
-    private Graphics[][] grid;
+    private JButton[][] grid; //grille de bouton
+    private int gridWidth;
+    private int gridLength;
     
-    public PanGraphic() {
+    public PanGraphic(int gridWidth, int gridLength) {
         this.setPreferredSize(new Dimension(600, 500));
+        this.gridLength = gridLength;
+        this.gridWidth = gridWidth;
+        this.Grid();
     }
     
-    @Override
-    public void paintComponent(Graphics g) {
-        int width = 255;
-        int length = 255;
-        
-        Graphics2D g2d = (Graphics2D) g;
-        this.setLayout(new GridLayout(width,length));
-        grid = new Graphics[width][length];
-        for(int y=0; y<length; y++)
+    private void Grid() {
+        this.setLayout(new GridLayout(this.gridWidth, this.gridLength));
+        grid = new JButton[this.gridWidth][this.gridLength];
+        for(int y=0; y<this.gridLength; y++)
         {
-            for(int x=0; x<width; x++)
+            for(int x=0; x<this.gridWidth; x++)
             {
-                //grid[x][y] = g
-                g2d.setColor(new Color(x, 127, y));
-                g2d.drawRect(x+5, y+5, 15, 15);
-                //g2d.fillRect(x, y, width, length);
-               /* 
-                g2d.draw(new Rectangle2D.Double(x, y, 100, 100));*/
+                //grid[x][y] = new JButton(x+","+y); //coordonnee - commenter la ligne suivante
+                grid[x][y] = new JButton(); //creation des bouttons
+                grid[x][y].setBackground(Color.white);
+                this.add(grid[x][y]); //ajout des boutons
             }
         }
-        g2d.setColor(Color.blue);
-        //g2d.fillRect(1, 1, 20, 20);
     }
 }
--- J-Sim Forest_Project/src/gil/PanMenu.java
+++ J-Sim Forest_Project/src/gil/PanMenu.java
@@ -1,14 +1,31 @@
 package gil;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PanMenu extends JPanel{
+    private JButton butSave;
     
     public PanMenu() {
         this.setPreferredSize(new Dimension(200, 200));
         JLabel libMenu = new JLabel("Menu");
         this.add(libMenu);
+        
+        this.butSave = new JButton("Save");
+        //Classes internes qui écoutent nos boutons
+        butSave.addActionListener(new ButSaveListener());
+        this.add(butSave);
+    }
+      
+    //Classe écoutant le bouton Save
+    class ButSaveListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            System.out.println("Save");
+        }
     }
 }
